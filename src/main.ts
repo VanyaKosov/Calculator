93ce9c1fc94a3c169bca2199466ae8c8d5fa4a67
Ivan Kosov <kosov.vanya@gmail.com>
2026-05-17 10:46:25 -0500

    Added verification for user input

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { approximateFunction } from './approxiamator';
 import { drawAxis, drawGraph } from './canvas';
-import type { Parameters } from './parameters';
+import { Parameters } from './parameters';
 import { parse } from './parser';
 
 const updateButton = document.getElementById("update_button")! as HTMLButtonElement;
 updateButton.onclick = updateOnClick;
+const errorMessage = document.getElementById("error_message")! as HTMLLabelElement;
 const xMin = document.getElementById("x_min")! as HTMLInputElement;
 const xMax = document.getElementById("x_max")! as HTMLInputElement;
 const yMin = document.getElementById("y_min")! as HTMLInputElement;
@@ -12,20 +13,24 @@ const yMax = document.getElementById("y_max")! as HTMLInputElement;
 const userEquation = document.getElementById("equation")! as HTMLInputElement;
 
 function updateOnClick() {
-	const equation = parse(userEquation.value);
-	console.log(equation);
+	errorMessage.textContent = "";
+	try {
+		const equation = parse(userEquation.value);
+		console.log(equation);
 
-	const params: Parameters = {
-		xMin: parseFloat(xMin.value),
-		xMax: parseFloat(xMax.value),
-		yMin: parseFloat(yMin.value),
-		yMax: parseFloat(yMax.value),
+		const params = new Parameters(
+			parseFloat(xMin.value),
+			parseFloat(xMax.value),
+			parseFloat(yMin.value),
+			parseFloat(yMax.value),
+		);
+
+		drawAxis(params);
+		const approximatedFunction = approximateFunction(equation, params, 1000);
+		drawGraph(params, approximatedFunction);
+	} catch (error) {
+		errorMessage.textContent = error as string;
 	}
-
-	drawAxis(params);
-	const approximatedFunction = approximateFunction(equation, params, 1000);
-	drawGraph(params, approximatedFunction);
-	console.log(approximatedFunction);
 }
 
 function main(): void {
--- src/parameters.ts
+++ src/parameters.ts
@@ -1,6 +1,20 @@
-export interface Parameters {
-    xMin: number;
-    xMax: number;
-    yMin: number;
-    yMax: number;
+export class Parameters {
+    public readonly xMin: number;
+    public readonly xMax: number;
+    public readonly yMin: number;
+    public readonly yMax: number;
+
+    public constructor(xMin: number, xMax: number, yMin: number, yMax: number) {
+        if (isNaN(xMin)) throw '"X min" must be defined';
+        if (isNaN(xMax)) throw '"X max" must be defined';
+        if (isNaN(yMin)) throw '"Y min" must be defined';
+        if (isNaN(yMax)) throw '"Y max" must be defined';
+        if (xMin >= xMax) throw '"X min" must be less than "X max"';
+        if (yMin >= yMax) throw '"Y min" must be less than "Y max"';
+
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+    }
 }
