7a27b37afbab72530ff2de6cc626949b48d2b9a2
Ivan Kosov <kosov.vanya@gmail.com>
2026-05-17 16:38:26 -0500

    Added test for approximator

--- src/approximator.test.ts
+++ src/approximator.test.ts
@@ -1,1 +1,13 @@
-// TODO
+import { describe, expect, test } from '@jest/globals'
+import type { Equation } from './parser';
+import { Parameters } from './parameters';
+import { approximateFunction, Pos } from './approxiamator'
+
+describe('parse', () => {
+    test.each([
+        [['x', 2, '^'], new Parameters(-5, 5, -5, 5), 10, [[new Pos(-3, 9), new Pos(-2, 4), new Pos(-1, 1), new Pos(0, 0), new Pos(1, 1), new Pos(2, 4), new Pos(3, 9)], []]],
+
+    ])('approximateFunction("%s", %s, %s) => %j', (equation: Equation, params: Parameters, numSteps: number, expected: Pos[][]) => {
+        expect(approximateFunction(equation, params, numSteps)).toEqual(expected);
+    })
+})
